6428be240170bb77028220711a04e2e3d6cd6d05
fdoruiztal <112428834+fdoruiztal@users.noreply.github.com>
2022-09-01 11:48:46 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    resta = resta - 1
+    basic.showNumber(resta)
+})
+input.onButtonPressed(Button.AB, function () {
+    texto = "adios"
+    basic.showString(texto)
+})
+input.onButtonPressed(Button.B, function () {
+    suma = suma + 1
+    basic.showNumber(suma)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(texto)
+})
+let resta = 0
+let suma = 0
+let texto = ""
+texto = "hola"
+suma = 1
+resta = 10
