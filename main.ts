3ec49a15b0121ccdf16b3e8f868fd1648e8ddc70
fdoruiztal <112428834+fdoruiztal@users.noreply.github.com>
2022-09-05 12:14:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    resta = resta - 1
-    basic.showNumber(resta)
+    suma = suma + 1
+    basic.showNumber(suma)
 })
 input.onButtonPressed(Button.AB, function () {
     texto = "adios"
     basic.showString(texto)
 })
 input.onButtonPressed(Button.B, function () {
-    suma = suma + 1
-    basic.showNumber(suma)
+    resta = resta - 1
+    basic.showNumber(resta)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     texto = "hola"
